0ead56c8c52973ded3cc003a26f52dc3e25e0554
hashi <sangichoigo@gmail.com>
2026-06-26 20:32:05 +0900

    Add files via upload

---
+++ stations.js
@@ -0,0 +1,13 @@
+// stations.js
+const allStations = [
+    { name: "京都", kana: "きょうと", subName: "なし", company: "JR西日本", line: "JR京都線, 琵琶湖線, 湖西線" },
+    { name: "京都", kana: "きょうと", subName: "なし", company: "JR東海", line: "東海道新幹線" },
+    { name: "京都", kana: "きょうと", subName: "なし", company: "近鉄", line: "近鉄京都線" },
+    { name: "京都", kana: "きょうと", subName: "なし", company: "京都市営地下鉄", line: "烏丸線" },
+    { name: "大阪", kana: "おおさか", company: "JR西日本", line: "大阪環状線, 東海道本線" },
+    { name: "梅田", kana: "うめだ", company: "Osaka Metro", line: "御筋線" },
+    { name: "谷町四丁目", kana: "たにまちよんちょうめ", company: "Osaka Metro", line: "谷町線/中央線" },
+    { name: "谷町六丁目", kana: "たにまちろくちょうめ", company: "Osaka Metro", line: "谷町線/長堀鶴見緑地線" },
+    // 副駅名（通称や施設名など）がある場合の例
+    { name: "国際会館", kana: "こくさいかいかん", subName: "同志社岩倉キャンパス前", company: "京都市営地下鉄", line: "烏丸線" }
+];
